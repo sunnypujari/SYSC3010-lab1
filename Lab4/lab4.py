287f9f0f43518a8205aa7066a8f25c3657d85209
sunnypujari <sunjeevanipujari@cmail.carleton.ca>
2020-10-08 08:57:39 -0400

    lab4 assesment

--- Lab4/lab4.py
+++ Lab4/lab4.py
@@ -1,1 +1,22 @@
+import urllib.request
+import requests
+import threading
+import json
 
+import random
+
+def thingspeak_post():
+    threading.Timer(15,thingspeak_post).start()
+    val1 ="L3-T-2"
+    val2="sunjeevanipujari@cmail.carleton.ca"
+    val3 = "b"
+    URl='https://api.thingspeak.com/update?api_key='
+    KEY=' AT68QYQ94MQEWXL3'
+    HEADER='&field1={}&field2={}&field3{}'.format(val1,val2,val3)
+    NEW_URL = URl+KEY+HEADER
+    print(NEW_URL)
+    data=urllib.request.urlopen(NEW_URL)
+    print(data)
+    
+if __name__ == '__main__':
+    thingspeak_post()
